29db06825e98f25d20c1da547a361c5cec69e316
Guilherme Souza <guivideojob@gmail.com>
2012-09-22 00:52:31 -0300

    refactoring tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,15 @@
-$:.unshift(File.dirname(__FILE__) + '/../lib')
-require 'ruby-thumbor'
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+
+if /^1\.8/ === RUBY_VERSION
+  require 'simplecov'
+
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+end
+
+RSpec.configure do |c| 
+  c.filter_run :focus => true
+  c.run_all_when_everything_filtered = true
+end
