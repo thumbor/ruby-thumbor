f4e2eabc40e273fcb3b095e4a3e12b20999cf9b6
Richard Lee <dlackty@gmail.com>
2014-12-10 23:14:39 +0800

    Remove should syntax setting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,11 @@
 require 'simplecov'
 
 SimpleCov.start do
-add_filter '/spec/'
+  add_filter '/spec/'
 end
 
 
 RSpec.configure do |c|
   c.filter_run :focus => true
   c.run_all_when_everything_filtered = true
-  c.expect_with :rspec do |config|
-    config.syntax = [:should, :expect]
-  end
 end
